73242948d18515a6b1772e642db454602d903e16
sevlord <sllord@mail.ru>
2012-11-20 11:38:06 +0200

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uwctemplate::Application.config.secret_token = 'c7b73b7cb715ac7ac985be3a8107ec7a95b2a55e4a6f7b59a7e3aeff056f5e38ca0ac13a292ad08cfdb7bc437b1a676c1d652b762f8313f13560576c368b6026'
